c49fea8997d5a1620de2075dd6dd3e3c415a74eb
Dalton Welch <dalton.welch@western.edu>
2018-01-24 16:57:44 -0700

    changed regular expression to only keep datetime and ip address of users in Parse,py

--- src/Parse.py
+++ src/Parse.py
@@ -7,7 +7,7 @@ from os.path import isfile, join
 import matplotlib
 import matplotlib.pyplot as plt
 
-regex = r"(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2})\s+[\d\w.]+\s([A-Z]+)\s+([^\s]+)\s+\S+\s+\d+\s+\S+\s([\d\w.]+)"
+regex = r"(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2})\s+[\d\w.]+\s+[A-Z]+\s+[^\s]+\s+\S+\s+\d+\s+\S+\s([\d\w.]+)"
 extractor = re.compile(regex)
 
 def parse_line(line):
@@ -17,7 +17,7 @@ def parse_line(line):
     pieces = extractor.match(line).groups()
     dt=datetime.datetime.strptime(pieces[0],"%Y-%m-%d %H:%M:%S")
     # make a tuple with dt and the rest (splatted)
-    return (dt, *pieces[1:])
+    return (dt, pieces[1])
 
 def parse_file(log_path):
     with open(log_path) as f:
@@ -25,4 +25,4 @@ def parse_file(log_path):
     lines = [l for l in lines if l[0]!="#"]
     return [parse_line(l) for l in lines]
 
-print(parse_file("WebLogs/TestLog.log"))
+print(parse_file("WebLogs/TestLog.log")[0])
